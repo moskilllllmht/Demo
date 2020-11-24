0671f4a33c5bf03d2d3cd7539d6376c667c4e8bc
moskilllllmht <thanhnguyen15022008@gmail.com>
2020-11-24 19:56:17 +0700

    test

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tinhtoan.h"
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a=100, b=7;
+	printf("\nTong hai so la: %d",cong(a,b));
+	printf("\nHieu hai so la: %d",tru(a,b));
+	printf("\nTich hai so la: %d",nhan(a,b));
+	printf("\nThuong hai so la: %f",chia(a,b));
+	return 0;
+}
